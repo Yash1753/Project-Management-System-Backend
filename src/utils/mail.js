94f3a0cae179a12dd6bb3a63156fbbbec03f9f11
Yash Rohilla <yr322005@gmail.com>
2025-12-23 21:57:23 +0530

    Send mail function is added

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,6 +1,45 @@
 import Mailgen from "mailgen";
-
+import nodemailer from "nodemailer"
 //generate mail
+
+const sendMail = async(options)=>{
+    const mailGenerator = new Mailgen({
+        theme : "default",
+        product : {
+            name : "taskmanager",
+            link : "https://taskmanager.com"
+        }
+    })
+
+    const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent)
+    const emailHtml = mailGenerator.generate(options.mailgenContent)
+
+    const transporter = nodemailer.createTransport({
+        host : process.env.MAILTRAP_SMTP_HOST,
+        port : process.env.MAILTRAP_SMTP_PORT,
+        auth : {
+            user : process.env.MAILTRAP_SMTP_USER,
+            pass : process.env.MAILTRAP_SMTP_PASSWORD
+        }
+
+    })
+
+    const mail = {
+        from : "mail.taskmanager@example.com",
+        to : options.email,
+        subject : options.subject,
+        text : emailTextual,
+        html : emailHtml
+    }
+
+    try{
+        await transporter.sendMail(mail)
+    }catch(error){
+        console.error("email service failed, MAILTRAP DEKHO APNA");
+        console.log("err : " , error)
+    }
+}
+
 
 const emailVerificationMailgenContent = function(username,verificationURL){
     return{
@@ -39,5 +78,6 @@ const forgotPasswordMailgenContent = function(username,passswordURL){
 
 export {
     emailVerificationMailgenContent,
-    forgotPasswordMailgenContent
+    forgotPasswordMailgenContent,
+    sendMail
 }
